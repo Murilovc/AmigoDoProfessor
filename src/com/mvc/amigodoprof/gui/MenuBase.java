b410c0911c55d370ee9c059546c0562db439e758
Murilovc <mvc.dev7cf@gmail.com>
2021-09-26 23:29:48 -0500

    alteracoes em MenuBase

--- src/com/mvc/amigodoprof/gui/MenuBase.java
+++ src/com/mvc/amigodoprof/gui/MenuBase.java
@@ -4,8 +4,23 @@ import javax.swing.JFrame;
 
 public class MenuBase extends JFrame{
 	
-	public MenuBase() {
+	private static ModoDeAcesso modo;
+	
+	private String idEntidadePai, idEntidadeAtual;
+	
+	private MenuBase menuPai;
+	
+	public MenuBase(ModoDeAcesso modo, MenuBase pai) {
 		
+		MenuBase.modo = modo;
+		
+		this.menuPai = pai;
+		
+		if(modo == ModoDeAcesso.RESTRITO) {
+			if(pai != null) {
+				idEntidadePai = pai.getIdEntidadeAtual();
+			}
+		}
 	}
 	
 	public enum ModoDeAcesso {
@@ -22,12 +37,41 @@ public class MenuBase extends JFrame{
 		 * um menu de atividades que só
 		 * exiba, adicione e edite atividades
 		 * para aquela aula, sem o usuário
-		 * precisar definir nada.*/
-		RESTRITO,
-		
+		 * precisar definir nada. */
+		RESTRITO("Restrito"),
 		
 		/* Abre o determinado menu 
 		 * de forma livre*/
-		GERAL;
+		GERAL("Geral");
+		
+		String descricao;
+		
+		ModoDeAcesso(String descricao) {
+			this.descricao = descricao;
+		}
+		
+		public ModoDeAcesso getModoDeAcesso() {
+			return modo;
+		}
+		
+		public void setModoDeAcesso(ModoDeAcesso modo) {
+			MenuBase.modo = modo;
+		}
+	}
+
+	public String getIdEntidadePai() {
+		return idEntidadePai;
+	}
+
+	public String getIdEntidadeAtual() {
+		return idEntidadeAtual;
+	}
+
+	public void setIdEntidadePai(String idEntidadePai) {
+		this.idEntidadePai = idEntidadePai;
+	}
+
+	public void setIdEntidadeAtual(String idEntidadeAtual) {
+		this.idEntidadeAtual = idEntidadeAtual;
 	}
 }
